8016a696c6d246392b46aae2d9184bf99b5233a8
ntrogh <ntrogh@hotmail.com>
2020-06-12 21:20:39 +0200

    Update koedo stats format

--- api/shared/stat-data.js
+++ api/shared/stat-data.js
@@ -1,55 +1,20 @@
 const data = {
   stats: [
     {
+      date: '2020-06-06T18:25:43.511Z',
+      count: 25
+    },
+    {
       date: '2020-06-05T18:25:43.511Z',
-      counts: [
-        {
-          koedo_id: 10,
-          count: 20
-        },
-        {
-          koedo_id: 20,
-          count: 7
-        },
-        {
-          koedo_id: 30,
-          count: 2
-        },
-      ]
+      count: 20
     },
     {
       date: '2020-06-04T18:25:43.511Z',
-      counts: [
-        {
-          koedo_id: 10,
-          count: 16
-        },
-        {
-          koedo_id: 20,
-          count: 6
-        },
-        {
-          koedo_id: 30,
-          count: 2
-        },
-      ]
+      count: 11
     },
     {
       date: '2020-06-03T18:25:43.511Z',
-      counts: [
-        {
-          koedo_id: 10,
-          count: 14
-        },
-        {
-          koedo_id: 20,
-          count: 4
-        },
-        {
-          koedo_id: 30,
-          count: 1
-        },
-      ]
+      count: 7
     },
   ],
 };
