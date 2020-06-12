e9549f78fd21320836771b2f1b5080aa9e7278d6
ntrogh <ntrogh@hotmail.com>
2020-06-13 00:32:52 +0200

    Revert to static data

--- api/shared/stat-data.js
+++ api/shared/stat-data.js
@@ -1,30 +1,30 @@
-const config = require("./cosmosdb-config");
-const CosmosClient = require("@azure/cosmos").CosmosClient;
-const KoedosDao = require("./koedosDao");
+// const config = require("./cosmosdb-config");
+// const CosmosClient = require("@azure/cosmos").CosmosClient;
+// const KoedosDao = require("./koedosDao");
 
-// const data = {
-//   stats: [
-//     {
-//       date: '2020-06-06T18:25:43.511Z',
-//       count: 25
-//     },
-//     {
-//       date: '2020-06-05T18:25:43.511Z',
-//       count: 20
-//     },
-//     {
-//       date: '2020-06-04T18:25:43.511Z',
-//       count: 11
-//     },
-//     {
-//       date: '2020-06-03T18:25:43.511Z',
-//       count: 7
-//     },
-//   ],
-// };
+const data = {
+  stats: [
+    {
+      date: '2020-06-06T18:25:43.511Z',
+      count: 25
+    },
+    {
+      date: '2020-06-05T18:25:43.511Z',
+      count: 20
+    },
+    {
+      date: '2020-06-04T18:25:43.511Z',
+      count: 11
+    },
+    {
+      date: '2020-06-03T18:25:43.511Z',
+      count: 7
+    },
+  ],
+};
 
 async function getStats() {
-  return "test";
+  // return "test";
   // try {
 
   //   const koedosDao = new KoedosDao(config.databaseId, config.statsContainerId);
@@ -39,7 +39,7 @@ async function getStats() {
   //   //   console.log(`${item.date} - ${item.count}`);
   //   // });
 
-  //   // return data.stats;
+  return data.stats;
   //   return items;
   // }
   // catch (e) {
