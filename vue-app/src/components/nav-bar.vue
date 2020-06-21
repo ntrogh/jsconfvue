9ae8360fcdee93fe0bb792597d610f80d6cdc1fe
ntrogh <ntrogh@hotmail.com>
2020-06-21 23:20:47 +0200

    fix

--- vue-app/src/components/nav-bar.vue
+++ vue-app/src/components/nav-bar.vue
@@ -26,7 +26,7 @@ export default {
         const response = await fetch('/.auth/me');
         const payload = await response.json();
         const { clientPrincipal } = payload;
-        console.log(userInfo);
+        console.log(clientPrincipal);
         return clientPrincipal;
       } catch (error) {
         console.error('No profile could be found');
