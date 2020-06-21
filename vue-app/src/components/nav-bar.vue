c337ce189790bca18026b7c3988d1d02ec231b61
ntrogh <ntrogh@hotmail.com>
2020-06-21 23:16:36 +0200

    Add logging

--- vue-app/src/components/nav-bar.vue
+++ vue-app/src/components/nav-bar.vue
@@ -26,6 +26,7 @@ export default {
         const response = await fetch('/.auth/me');
         const payload = await response.json();
         const { clientPrincipal } = payload;
+        console.log(userInfo);
         return clientPrincipal;
       } catch (error) {
         console.error('No profile could be found');
